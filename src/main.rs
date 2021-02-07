c91e5b7b2fd422e1550991fc10e55c4c348204dc
Spyros Roum <spyrosr1@gmail.com>
2021-02-07 19:06:25 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-use cli::commands::run;
+mod gif;
+mod quote;
 
-mod cli;
-mod requests;
+use clipboard_ext::{clipboard::ClipboardProvider, x11_fork::ClipboardContext};
 
-fn main() {
-    run();
+use anyhow::Result;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    /// Retrieves Star Wars Quotes
+    Quote { keywords: String },
+    /// Retrieves Star Wars GIFs
+    Gif { keywords: String },
+}
+
+fn main() -> Result<()> {
+    let opt: Opt = Opt::from_args();
+
+    let r = match opt {
+        Opt::Quote { keywords } => quote::get_quote(keywords.as_str()),
+        Opt::Gif { keywords } => gif::get_gif(keywords.as_str()),
+    }?;
+
+    println!("{}\n", r);
+
+    let mut ctx: ClipboardContext = ClipboardProvider::new().expect("Failed to open clipboard");
+    ctx.set_contents(r)
+        .expect("Failed to set clipboard contents");
+
+    println!("Added to your clipboard!");
+
+    Ok(())
 }
---
+++ src/quote.rs
@@ -0,0 +1,17 @@
+use rand::seq::IteratorRandom;
+
+use anyhow::{Context, Result};
+
+pub(crate) const QUOTES: &[&str] = &include!(concat!(env!("OUT_DIR"), "/quotes.rs"));
+
+pub fn get_quote(keywords: &str) -> Result<String> {
+    let keywords = keywords.to_lowercase();
+    let mut rng = rand::thread_rng();
+
+    QUOTES
+        .iter()
+        .filter(|q| q.to_lowercase().contains(keywords.as_str()))
+        .choose(&mut rng)
+        .map(|q| q.to_string())
+        .context("No quote found for those keywords.")
+}
